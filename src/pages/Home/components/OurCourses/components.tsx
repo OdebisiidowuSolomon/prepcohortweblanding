c4a16e80b81106f4f5c90de0ab2427a8cdfac1d7
unknown <odebisiidowu09@gmail.com>
2024-06-02 11:29:03 +0100

    feat:added All course slider ui

---
+++ src/pages/Home/components/OurCourses/components.tsx
@@ -0,0 +1,75 @@
+export const courseData = [
+  { id: 1, imgSrc: "/images/waec-img.png", label: "WAEC" },
+  { id: 2, imgSrc: "/images/jamb-img.png", label: "JAMB" },
+  { id: 3, imgSrc: "/images/ielts-img.png", label: "IELTS" },
+  { id: 4, imgSrc: "/images/jamb-img.png", label: "NABTEB" },
+  { id: 5, imgSrc: "/images/neco-img.png", label: "NECO" },
+];
+
+export const _CourseItem = ({
+  imageUrl,
+  label,
+}: {
+  imageUrl: string;
+  label: string;
+}) => {
+  return (
+    <div className="item bg-white">
+      <div className="category-one__item flex flex-col items-center">
+        <div className="w-[230px] h-[150px]">
+          <img
+            src={imageUrl}
+            alt={imageUrl}
+            className="object-contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+        <p className="p-0 m-0">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export const CourseItem = ({
+  imageUrl,
+  label,
+}: {
+  imageUrl: string;
+  label: string;
+}) => {
+  return (
+    <div className="item bg-white w-[300px]">
+      <div className="category-one__item">
+        <div className="w-full h-[150px]">
+          <img
+            src={imageUrl}
+            alt={imageUrl}
+            className="object-contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+
+        <div className="p-4">
+          <div className="flex justify-between items-center my-3">
+            <div>
+              <p className="p-0 m-0 font-medium">Biology</p>
+              <p className="p-0 m-0 text-sm">Institution name</p>
+            </div>
+            <span className="bg-green-400 text-sm p-1 font-medium">New</span>
+          </div>
+
+          <div className="flex justify-between items-center mt-4">
+            <div className="flex items-center">
+              <p className="p-0 m-0 text-sm">4.5</p>
+              <i className="fa fa-star ml-3 text-yellow-300 text-sm"></i>
+            </div>
+            <div className="flex items-center">
+              <i className="fa fa-users text-sm mr-2"></i>
+              <p className="p-0 m-0">202</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
